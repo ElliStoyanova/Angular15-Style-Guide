b2de50e0bf9696e5a11356450111fd9643ffe0e8
ElliStoyanova <elli.debelli@gmail.com>
2024-11-21 14:41:13 +0200

    migrating app to standalone components

--- angular15-style-guide/src/app/app.component.ts
+++ angular15-style-guide/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [MatSlideToggleModule, NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
 })
 export class AppComponent {
   title = 'angular15-custom-theme-app';
