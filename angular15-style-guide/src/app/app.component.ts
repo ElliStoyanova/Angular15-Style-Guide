5b732043e7fa42581f9a889973602db16aea3b22
ElliStoyanova <elli.debelli@gmail.com>
2024-11-22 17:22:24 +0200

    wip - add button, form-field and toolbar styles

--- angular15-style-guide/src/app/app.component.ts
+++ angular15-style-guide/src/app/app.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgSwitch, NgSwitchDefault, NgSwitchCase} from '@angular/common';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: true,
-    imports: [MatSlideToggleModule, NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet]
+    imports: [
+      NgSwitch, 
+      NgSwitchDefault, 
+      NgSwitchCase, 
+      RouterOutlet,
+      MatSlideToggleModule,
+      MatButtonModule,
+      MatDividerModule,
+      MatInputModule,
+      MatFormFieldModule,
+      FormsModule, 
+      MatFormFieldModule, 
+      MatInputModule, 
+      ReactiveFormsModule,
+      MatToolbarModule, 
+      MatIconModule
+    ]
 })
 export class AppComponent {
-  title = 'angular15-custom-theme-app';
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 }
